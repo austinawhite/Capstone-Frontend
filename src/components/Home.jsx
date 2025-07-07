37daa338c5acf1d09e396dc491c938ea65461cd6
Austina White <austinawhite@gmail.com>
2025-07-07 14:50:47 -0400

    basic homepage

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import { React, useEffect } from 'react';
+
+const Home = () => {
+
+    return (
+        <>
+        <div> 
+        <h1> Homepage </h1> 
+        </div>
+        </>
+    );
+};
+
+export default Home;

3111e10fbf297e06aaf0a79559caeaa6611c3d21
Maurice Drew <drewm1987@gmail.com>
2025-07-09 20:49:20 -0500

    added destinations component, listed as under construction with city details link

---
+++ src/components/Destinations.jsx
@@ -0,0 +1,20 @@
+
+import CityDetails from "./CityDetails";
+import Experiences_Drink from "./ExperienceDrink";
+
+
+const Destinations = () => {
+    
+    return (
+        <>
+        <h1>Under Construction</h1>
+        <CityDetails> </CityDetails>
+        </>
+         
+           
+        
+    );
+};
+
+export default Destinations;
+

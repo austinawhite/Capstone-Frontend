42c2d3f6cee086fdc6d6e6ad7940081895f72036
Austina White <austinawhite@gmail.com>
2025-07-10 10:55:16 -0400

    added my account page

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,2 +1,50 @@
-import { React, useEffect } from 'react';
-import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+
+export default function Account ({token}) {
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+    if (!token) {
+      navigate("/login");
+      return;
+    }
+
+    async function getUser() {
+      try {
+        const result = await fetch(`http://localhost:3000/users/user`, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            },
+        });
+        const data = await result.json();
+        setUser(data);
+      } catch (error) {
+        console.error("Error loading user details: ", error);
+      }
+    }
+    getUser();
+  }, []);
+
+   return(
+    <>
+    <div className="account-container">
+     {user ? (
+    <>
+      <h1>My Account</h1>
+      <h3>Email Address: {user.email} </h3>
+
+      <h1> My Upcoming Trips </h1>
+
+      <h1> My Previous Trips </h1>
+      
+    </>
+  ) : (
+    <p> Sorry, we are having issues loading your account. </p>
+  )}
+</div>
+</>
+)
+
+}

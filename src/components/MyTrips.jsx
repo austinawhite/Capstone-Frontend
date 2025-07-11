03d43457298b862d207bbf9deaef9c47a4edf140
Maurice Drew <drewm1987@gmail.com>
2025-07-10 22:16:29 -0500

    just added the foundation to my trips jsx

--- src/components/MyTrips.jsx
+++ src/components/MyTrips.jsx
@@ -1,4 +1,14 @@
-// My Trips - Shows a list of all trips for a user. This component is used on the my account page. 
 
-import { React, useEffect } from 'react';
-import { useParams, useNavigate } from "react-router-dom";
+
+
+function MyTrips (){
+   
+
+    return (
+        <>
+       
+        </>
+    )
+}
+
+export default MyTrips
